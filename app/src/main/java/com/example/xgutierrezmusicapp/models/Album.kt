157def2c7ea7bfad4b62edbf4d39401d000472c4
Xitony0407 <gutierrezperezximena4@gmail.com>
2025-10-08 20:21:13 -0600

    Data: Añadido permiso de INTERNET y creado el modelo de datos Album.

---
+++ app/src/main/java/com/example/xgutierrezmusicapp/models/Album.kt
@@ -0,0 +1,12 @@
+package com.example.xgutierrezmusicapp.models
+
+import java.io.Serializable
+
+data class Album(
+    val id: String,
+    val title: String,
+    val artist: String,
+    val cover: String,
+    val description: String? = null,
+    val songs: List<String>? = null
+) : Serializable
